bc56a1072148a91859710c5f9dac68630f4d92ae
Simone Scheuer <simones@ada.cs.pdx.edu>
2025-01-22 13:35:48 -0800

    committing because i cant read

--- Lab2/1000-digit.c
+++ Lab2/1000-digit.c
@@ -13,37 +13,41 @@ int main(void)
 
     printf("\nTHIS IS FROM INSIDE MAIN\n");
 
-    while (fgets(buffer, STRLEN, stdin) != NULL)
+    while (fgets(buffer, STRLEN, stdin) != NULL) //continually read in
     {
-        printf("Reading line: %s", buffer);
-        len = strlen(buffer);
-        char temphex[7] = {0};
-        int num_seq_hex = 0;
-        for(int i = 0; i < len; i++)
+        printf("Reading line: %s", buffer); //print the line you are reading
+        len = strlen(buffer); //set len to the length of the string in buffer
+        char temphex[7] = {0}; //create a temphex big enough for 6 chars and a null terminator 
+        int num_seq_hex = 0; //initate the index to track how many sequential hexes you have
+                             
+        for(int i = 0; i < len; i++) //PROCEED THROUGH EVERY CHAR
         { 
-            if (isxdigit(buffer[i]))
+            if (isxdigit(buffer[i])) //IF IT IS A HEX, BEGIN THE PROCESS
             {
-                printf("%c ", buffer[i]);
-                if (num_seq_hex < 6)
+                printf("%c ", buffer[i]); //Output it for clarity
+                if (num_seq_hex == 6) //IF YOU HAVE ACCUMULATED SIX, AND YOU ARE LOOKING AT A HEX, TIME TO SHIFT AND MAKE A NEW SET
                 {
-                    num_seq_hex += 1;
-                    temphex[num_seq_hex-1] = buffer[i];
-                }
-                if (num_seq_hex == 6)
-                {
-                     temphex[6] = '\0';
-                    printf("\nSix concurrent hex found: ");
-                    for(int i = 0; i < 6; i++)
+                    for (int j = 0; j < 5; j++) //for all but one of the chars in the hex sequence..
                     {
-                        printf("%c", temphex[i]);
-                        num_seq_hex = 0;
+                        temphex[j] = temphex[j + 1]; //cut off the first char in the sequence
                     }
-                    printf("\n");
+                    temphex[5] = buffer[i]; //assign the sixth position to the new one.
+                }
+                else //IF YOU ARE NOT AT THE SIXTH POSITION OF A SEQUENCE
+                {
+                    temphex[num_seq_hex] = buffer[i]; //add the hex to the sequence
+                    num_seq_hex++; //increase the sequence by one
+                }
+                if (num_seq_hex == 6) //IF YOU HAVE SIX
+                {
+                    temphex[6] = '\0'; //set the SEVENTH POSITION, to the null terminator
+                    printf("\nSix concurrent hex found: "); //output the chain you found
+                    printf("%s", temphex);
                 }
             }
-            else
+            else //IF IT IS NOT A HEX
             {
-                num_seq_hex = 0;
+                num_seq_hex = 0; //reset the sequence.
             }
 
         }
@@ -51,5 +55,3 @@ int main(void)
     }
     return 0;
 }
-
-
